a56484f6923ad4b870dc8af3921b48f0ad5b848b
sder12 <sara.cetto@gmail.com>
2022-12-19 14:50:38 +0100

    Deciding type of info

--- database/migrations/2022_12_19_133408_create_trains_table.php
+++ database/migrations/2022_12_19_133408_create_trains_table.php
@@ -15,9 +15,41 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
+            $table->string('company', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+
+            $table->timestamp('departure_time');
+            $table->timestamp('arrival_time');
+
+            $table->tinyInteger('code_train')->unsigned();
+            $table->tinyInteger('carriage')->unsigned()->nullable();
+            $table->tinyInteger('is_on_time')->default(1);
+            $table->tinyInteger('got_canceled')->default(0);
+
             $table->timestamps();
         });
     }
+
+    //ID
+
+    // Azienda -> stringa-> varchar 80-100
+    // Stazione di partenza ->  stringa-> varchar 100
+    // Stazione di arrivo -> stringa-> varchar 100
+
+    // Orario di partenza -> time -> data + ora -> timestamp
+    // Orario di arrivo -> time -> data + ora -> timestamp
+
+    // Codice Treno -> tinyint unsigned
+    // Numero Carrozze -> number -> tinyint unsigned nullable 
+    // In orario -> boolean -> tinyint unsigned notnull -> default(1)
+    // Cancellato -> boolean -> tinyint unsigned -> defauly (0)
+
+    //TIMESTAMPS
+
+
+
 
     /**
      * Reverse the migrations.
